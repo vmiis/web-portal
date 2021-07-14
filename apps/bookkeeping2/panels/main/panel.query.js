a8c29ad32f38f7a107603022c6adbe053552c491
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-07-14 20:09:04 +1000

    update

--- apps/bookkeeping2/panels/main/panel.query.js
+++ apps/bookkeeping2/panels/main/panel.query.js
@@ -2,12 +2,12 @@ var vm_data={
     "income_expense":{
         "module":"vm-query",
         "api":"wapp",
-        "name":"Websystem 2018-2021 income/expense",
+        "name":"Websystem 2018-2022 income/expense",
         "table":"",
         "query":
         [
             {
-                "$match":{"I1": { "$gte": "2018-07-01", "$lt":"2021-07-01"}}
+                "$match":{"I1": { "$gte": "2018-07-01", "$lt":"2022-07-01"}}
             },
             {
                 "$project": {
@@ -57,12 +57,138 @@ var vm_data={
             {
                 "$project": {
                     "Data.Financial Year":"$FY",
-                    "Data.Inccome":"$Income",
+                    "Data.Income":"$Income",
                     "Data.Expense":"$Expense",    
                     "Data.P/L":"$P/L"    
                 }
             }
-        ]
+        ],
+        "options":"Income|DOLLAR2,Expense|DOLLAR2,P/L|DOLLAR2"
     },
     //-------------------------------
+    "BAS":{
+        "module":"vm-query",
+        "api":"wapp",
+        "name":"2018-2022 ATO BAS quarterly",
+        "table":"",
+        "query":
+        [
+            {
+                "$match":{
+                    "I1": { "$gte": "2018-07-01", "$lt":"2021-07-02"}
+                }
+            },
+            {
+                "$project": {
+                    "Years": { $substr: [ "$I1", 0, 4] },
+                    "M": { $substr: [ "$I1", 5, 2] },
+                    "Data.G1_sales":1,
+                    "Data.1A_GST":1,
+                    "Data.1B_GST":1,
+                    "Data.W1_salary":1,
+                    "Data.W2_withheld":1,
+                }
+            },
+            {
+                "$project": {
+                    "Years":1,
+                    "Data.G1_sales":1,
+                    "Data.1A_GST":1,
+                    "Data.1B_GST":1,
+                    "Data.W1_salary":1,
+                    "Data.W2_withheld":1,
+                    "Quarter":{
+                        "$switch": {
+                        branches: [
+                            { case: {$eq: ["$M", "01"]}, then: 1 },
+                            { case: {$eq: ["$M", "02"]}, then: 1 },
+                            { case: {$eq: ["$M", "03"]}, then: 1 },
+                            { case: {$eq: ["$M", "04"]}, then: 2 },
+                            { case: {$eq: ["$M", "05"]}, then: 2 },
+                            { case: {$eq: ["$M", "06"]}, then: 2 },
+                            { case: {$eq: ["$M", "07"]}, then: 3 },
+                            { case: {$eq: ["$M", "08"]}, then: 3 },
+                            { case: {$eq: ["$M", "09"]}, then: 3 },
+                            { case: {$eq: ["$M", "10"]}, then: 4 },
+                            { case: {$eq: ["$M", "11"]}, then: 4 },
+                            { case: {$eq: ["$M", "12"]}, then: 4 },
+                        ],
+                        default: 0
+                        }                
+                    },
+                }    
+            },
+            {
+                "$group":{
+                    "_id":{"Year":"$Years","Quarter":"$Quarter"},
+                    "G1 sales": { "$sum":"$Data.G1_sales"},
+                    "1A GST": { "$sum":"$Data.1A_GST"},
+                    "1B GST": { "$sum":"$Data.1B_GST"},
+                    "W1 salary": { "$sum":"$Data.W1_salary"},
+                    "W2_withheld": { "$sum":"$Data.W2_withheld"},
+                }
+            },
+            {
+                "$sort":{
+                    "_id":-1
+                }  
+            },
+            {
+                "$project": {
+                    "Data.Year":"$_id.Year",
+                    "Data.Quarter":"$_id.Quarter",
+                    "Data.G1 sales":"$G1 sales",
+                    "Data.1A GST":"$1A GST",
+                    "Data.1B GST":"$1B GST",    
+                    "Data.W1 salary":"$W1 salary",
+                    "Data.W2_withheld":"$W2_withheld",    
+                }
+            }
+        ],
+        "options":"G1 sales|DOLLAR2,1A GST|DOLLAR2,1B GST|DOLLAR2,W1 salary|DOLLAR2,W2_withheld|DOLLAR2"
+    },
+    //-------------------------------
+    "income_expense_monthly":{
+        "module":"vm-query",
+        "api":"wapp",
+        "name":"2018-2022 income/expense monthly",
+        "table":"",
+        "query":
+        [
+            {
+                "$match":{
+                    "I1": { "$gte": "2018-07-01", "$lt":"2021-07-02"}
+                }
+            },
+            {
+                "$project": {
+                    "Years": { $substr: [ "$I1", 0, 4] },
+                    "Month": { $substr: [ "$I1", 5, 2] },
+                    "Data.Income":1,
+                    "Data.Expense":1,
+                }
+            },
+            {
+                "$group":{
+                    "_id":{"Year":"$Years","Month":"$Month"},
+                    "Income": { "$sum":"$Data.Income"},
+                    "Expense": { "$sum":"$Data.Expense"},
+                }
+            },
+            {
+                "$sort":{
+                    "_id":-1
+                }  
+            },
+            {
+                "$project": {
+                    "Data.Year":"$_id.Year",
+                    "Data.Month":"$_id.Month",
+                    "Data.Income":"$Income",
+                    "Data.Expense":"$Expense",    
+                }
+            }
+        ],
+        "options":"Income|DOLLAR2,Expense|DOLLAR2"
+    }
 }
--- panels/main/module-list-bookkeeping2.js
+++ panels/main/module-list-bookkeeping2.js
@@ -9,7 +9,56 @@
     var pre_item="vm-demo-predefined-transaction-item";
     var transaction="vm-demo-transaction-records";
 
-    m[p+"panel"]=                                    {url:A+"/panels/main/panel.html", pre_item:pre_item, transaction:transaction, prefix:p, router:1};
+    m[p+"panel"]=                                    {url:A+"/panels/main/panel.html", pre_item:pre_item, transaction:transaction, prefix:p, router:1, company:"Demo"};
+    m[p+"predefined_transaction_item_data"]=         {url:H+"/predefined-transaction-item/data.html",                 api:api,Table:pre_item,form_module:"predefined_transaction_item_form",router:1};
+    m[p+"predefined_transaction_item_form"]=         {url:H+"/predefined-transaction-item/form.html",                 api:api,Table:pre_item};
+    m[p+"transaction-data-basic"]=                   {url:H+"/transaction-records/data-basic.html",                   api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-data-date"]=                    {url:H+"/transaction-records/data-date.html",                    api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-data-year-quarter"]=            {url:H+"/transaction-records/data-year-quarter.html",            api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-data-bas"]=                     {url:H+"/transaction-records/data-bas.html",                     api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-data-year-quarter-bas"]=        {url:H+"/transaction-records/data-year-quarter-bas.html",        api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-data-tax-return"]=              {url:H+"/transaction-records/data-tax-return.html",              api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-data-year-quarter-tax-return"]= {url:H+"/transaction-records/data-year-quarter-tax-return.html", api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-form"]=                         {url:H+"/transaction-records/form.html",                         api:api,Table:transaction,item_table:pre_item,router:1};
+    //-------------------------------------------------------------------------------------
+})();
+(function(){
+    //-------------------------------------------------------------------------------------
+    var p="bookkeeping2-wapp-"; //put a unique prefix to avoid conflict with others 
+    var A=$vm.hosting_path+"/apps/bookkeeping2";
+    var H=$vm.hosting_path+"/apps/bookkeeping2/modules";
+    var m=$vm.module_list;
+    var api="wapp";
+    
+    var pre_item="predefined-transaction-item-wappsystem";
+    var transaction="transaction-wappsystem";
+
+    m[p+"panel"]=                                    {url:A+"/panels/main/panel.html", pre_item:pre_item, transaction:transaction, prefix:p, router:1, company:"WAPP"};
+    m[p+"predefined_transaction_item_data"]=         {url:H+"/predefined-transaction-item/data.html",                 api:api,Table:pre_item,form_module:"predefined_transaction_item_form",router:1};
+    m[p+"predefined_transaction_item_form"]=         {url:H+"/predefined-transaction-item/form.html",                 api:api,Table:pre_item};
+    m[p+"transaction-data-basic"]=                   {url:H+"/transaction-records/data-basic.html",                   api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-data-date"]=                    {url:H+"/transaction-records/data-date.html",                    api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-data-year-quarter"]=            {url:H+"/transaction-records/data-year-quarter.html",            api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-data-bas"]=                     {url:H+"/transaction-records/data-bas.html",                     api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-data-year-quarter-bas"]=        {url:H+"/transaction-records/data-year-quarter-bas.html",        api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-data-tax-return"]=              {url:H+"/transaction-records/data-tax-return.html",              api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-data-year-quarter-tax-return"]= {url:H+"/transaction-records/data-year-quarter-tax-return.html", api:api,Table:transaction,form_module:"transaction-form",router:1};
+    m[p+"transaction-form"]=                         {url:H+"/transaction-records/form.html",                         api:api,Table:transaction,item_table:pre_item,router:1};
+    //-------------------------------------------------------------------------------------
+})();
+
+(function(){
+    //-------------------------------------------------------------------------------------
+    var p="bookkeeping2-vm-"; //put a unique prefix to avoid conflict with others 
+    var A=$vm.hosting_path+"/apps/bookkeeping2";
+    var H=$vm.hosting_path+"/apps/bookkeeping2/modules";
+    var m=$vm.module_list;
+    var api="wapp";
+    
+    var pre_item="predefined-transaction-item-vmautomation";
+    var transaction="transaction-vmautomation";
+
+    m[p+"panel"]=                                    {url:A+"/panels/main/panel.html", pre_item:pre_item, transaction:transaction, prefix:p, router:1, company:"VM"};
     m[p+"predefined_transaction_item_data"]=         {url:H+"/predefined-transaction-item/data.html",                 api:api,Table:pre_item,form_module:"predefined_transaction_item_form",router:1};
     m[p+"predefined_transaction_item_form"]=         {url:H+"/predefined-transaction-item/form.html",                 api:api,Table:pre_item};
     m[p+"transaction-data-basic"]=                   {url:H+"/transaction-records/data-basic.html",                   api:api,Table:transaction,form_module:"transaction-form",router:1};
