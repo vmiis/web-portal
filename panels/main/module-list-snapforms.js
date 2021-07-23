2e768220af9052b5ba169799c62851f8a8d7fcd1
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-07-23 11:13:12 +1000

    update

--- panels/main/module-list-snapforms.js
+++ panels/main/module-list-snapforms.js
@@ -1,9 +1,13 @@
 (function(){
     //-------------------------------------------------------------------------------------
-    var p=""; //put a unique prefix to avoid conflict with others 
-    var H=$vm.hosting_path+"/modules";
+    var p="snapforms-"; //put a unique prefix to avoid conflict with others 
+    var A=$vm.hosting_path+"/apps/snapforms";
+    var H=$vm.hosting_path+"/apps/snapforms/modules";
     var m=$vm.module_list;
     var api="wzd";
+    m[p+"panel"]  ={url:A+"/panels/main/panel.html", prefix:p, router:1};
     m[p+"contact"]={url:H+"/business/contact.data.html", api:api, router:1};
     //-------------------------------------------------------------------------------------
 })();
+
+
