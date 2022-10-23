110673caaea5381f4b520c937f812d38f417a9ca
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-10-23 15:11:48 +1100

    update

---
+++ apps/microapp/modules/module-list.js
@@ -0,0 +1,22 @@
+(function(){
+    //-------------------------------------------------------------------------------------
+    var H=$vm.hosting_path+"/modules";
+    var m=$vm.module_list;
+    //-------------------------------------------------------------------------------------
+    m["revenue-2022-data"]                        ={url:H+"/tasks/Use Cases/*/page.html",router:1};
+    m["revenue-2022-chart"]                       ={url:H+"/tasks/Use Cases/*/page.html",router:1};
+    m["What is a Micro-app"]                      ={url:H+"/tasks/About Microapp/*/page.html",router:1};
+    m["Cost of running a Micro-app"]              ={url:H+"/tasks/About Microapp/*/page.html",router:1};
+    m["Authentication"]                           ={url:H+"/tasks/About Microapp/*/page.html",router:1};
+    m["Permissions Control"]                      ={url:H+"/tasks/About Microapp/*/page.html",router:1};
+    m["Student Data"]                             ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-student",form_module:"Student Form",router:1};
+    m["Student Form"]                             ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-student"};
+    m["Membership-Application Data"]              ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-membership-application",form_module:"Membership-Application Form",router:1};
+    m["Membership-Application Form"]              ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-membership-application"};
+    
+    for(p in m){  
+        m[p].url=m[p].url.replace('*',p); 
+        var a=p.substring(0,p.lastIndexOf(' ')) +'/'+p.substring(p.lastIndexOf(' ') + 1); //replace the last space to /, example: Student Form --> Student/Form
+        m[p].url=m[p].url.replace('#',a); 
+    }
+})();
