fb8eae109249bc19759aed4420d2a7bda80e5ead
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-10-24 14:38:43 +1100

    update

--- apps/microapp/modules/module-list.js
+++ apps/microapp/modules/module-list.js
@@ -9,14 +9,18 @@
     m["Cost of running a Micro-app"]              ={url:H+"/tasks/About Microapp/*/page.html",router:1};
     m["Authentication"]                           ={url:H+"/tasks/About Microapp/*/page.html",router:1};
     m["Permissions Control"]                      ={url:H+"/tasks/About Microapp/*/page.html",router:1};
-    m["Student Data"]                             ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-student",form_module:"Student Form",router:1};
-    m["Student Form"]                             ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-student"};
-    m["Membership-Application Data"]              ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-membership-application",form_module:"Membership-Application Form",router:1};
-    m["Membership-Application Form"]              ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-membership-application"};
+    m["Student-Data"]                             ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-student",form_module:"Student-Form",router:1};
+    m["Student-Form"]                             ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-student"};
+    m["Membership-Application-Data"]              ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-membership-application",form_module:"Membership-Application-Form",router:1};
+    m["Membership-Application-Form"]              ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-membership-application"};
+    m["File-Data"]                                ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-file",form_module:"File-Form",router:1};
+    m["File-Form"]                                ={url:H+"/tasks/Use Cases/#/page.html",Table:"demo-file"};
     
     for(p in m){  
         m[p].url=m[p].url.replace('*',p); 
-        var a=p.substring(0,p.lastIndexOf(' ')) +'/'+p.substring(p.lastIndexOf(' ') + 1); //replace the last space to /, example: Student Form --> Student/Form
+        //var a=p.substring(0,p.lastIndexOf(' ')) +'/'+p.substring(p.lastIndexOf(' ') + 1); //replace the last space to /, example: Student Form --> Student/Form
+        //m[p].url=m[p].url.replace('#',a); 
+        var a=p.substring(0,p.lastIndexOf('-')) +'/'+p.substring(p.lastIndexOf('-') + 1); //replace the last - to /, example: Student-Form --> Student/Form
         m[p].url=m[p].url.replace('#',a); 
     }
 })();
