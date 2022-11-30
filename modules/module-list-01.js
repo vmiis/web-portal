e3d1785b480f709dae56110c185462582a22b551
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-11-30 16:15:36 +1100

    update

--- modules/module-list-01.js
+++ modules/module-list-01.js
@@ -355,11 +355,11 @@
         
         "abcjs-local":{url:$H+"/abcjs-local/page.html",router:1},
         "abcjs-local_waterfall":{url:$H+"/abcjs-local_waterfall/page.html"},
-        
         "abcjs-numbered":{url:$H+"/abcjs-numbered/page.html",router:1},
         "abcjs-numbered-local":{url:$H+"/abcjs-numbered-local/page.html",router:1},
         "sheet-music":{url:H+"/panels/sheet-music.html",router:1},
         "piano-left-hand-pattern":{url:H+"/panels/piano-left-hand-pattern.html",router:1},
+        "numbered-left-hand":{url:H+"/panels/numbered-left-hand/page.html","title":"Left hand pattern"},
         "ssl-check":{url:$H+"/ssl-check/page.html",router:1},
         "domain-check":{url:$H+"/domain-check/page.html",router:1},
         "api-server-status":{url:$H+"/api-server-status/page.html",router:1},
